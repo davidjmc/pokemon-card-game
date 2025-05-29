670c2d9d54422abbe0ab78effc6f9f513553398b
David Cavalcanti <david.junio@gmail.com>
2025-05-29 09:31:46 -0300

    Get Pokemon by name.

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ function App() {
   const [pokemon, setPokemon] = useState([null]);
   const [input, setInput] = useState('');
 
-  const fetchPokemon = async () => {
+  const buscarPokemon = async () => {
     if (!input) return;
 
     try {
       const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${input.toLowerCase()}`);
+      
       setPokemon({
         name: response.data.name,
         image: response.data.sprites.front_default,
         types: response.data.types.map(t => t.type.name).join(', ')
       });
+
     } catch (error) {
       alert('Pokémon não encontrado!');
       setPokemon(null);
@@ -31,7 +33,7 @@ function App() {
         placeholder="Digite o Nome ou número"
         style={{ padding: '0.5rem', fontSize: '1rem' }}
       />
-      <button onClick={fetchPokemon} style={{ marginLeft: '1rem', padding: '0.5rem 1rem' }}>
+      <button onClick={buscarPokemon} style={{ marginLeft: '1rem', padding: '0.5rem 1rem' }}>
         Buscar
       </button>
 
